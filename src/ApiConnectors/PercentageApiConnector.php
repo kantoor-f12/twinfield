3370ded2ce3134849534b4d6ef9c219c998ab2c6
Kelvin Souer <kelvinsouer.ks@gmail.com>
2017-12-29 15:01:33 +0100

    changed percentage to look more like the other calls

--- src/ApiConnectors/PercentageApiConnector.php
+++ src/ApiConnectors/PercentageApiConnector.php
@@ -2,19 +2,23 @@
 namespace PhpTwinfield\ApiConnectors;
 
 use PhpTwinfield\Mappers\PercentageMapper;
+use PhpTwinfield\Office;
 use PhpTwinfield\Request\Read\VatDataTransfer;
 
 class PercentageApiConnector extends ProcessXmlApiConnector
 {
     public function getPercentage(
-        string $office,
-        string $code
+        string $code,
+        Office $office
     ){
-        $vatInformation = new VatDataTransfer(
-            $office,
+        $vatInformation = new VatDataTransfer();
+        $vatInformation->setOffice($office->getCode())
+            ->setCode($code);
+        $response = $this->sendDocument($vatInformation);
+
+        return PercentageMapper::map(
+            $response,
             $code
         );
-        $response = $this->sendDocument($vatInformation);
-        return PercentageMapper::map($response, $code);
     }
 }
--- src/Request/Read/VatDataTransfer.php
+++ src/Request/Read/VatDataTransfer.php
@@ -1,27 +1,41 @@
 <?php
 namespace PhpTwinfield\Request\Read;
 
- use PhpTwinfield\Office;
+use PhpTwinfield\Office;
 
 class VatDataTransfer extends Read
 {
-    public function __construct($office, $code) {
+    public function __construct(Office $office = null, $code = null)
+    {
         parent::__construct();
-        $this->add('type', 'vat');
-        $this->add('office', $office);
-        $this->add('code', $code);
+        $this->add(
+            'type',
+            'vat'
+        );
+        if (null !== $office) {
+            $this->setOffice($office->getCode());
+        }
+
+        if (null !== $code) {
+            $this->setCode($code);
+        }
     }
 
     /**
      * Sets the office code for this vat request.
      *
      * @access public
-     * @param Office $office
+     *
+     * @param string $office
+     *
      * @return \PhpTwinfield\Request\Read\VatDataTransfer
      */
-    public function setOffice(Office $office)
+    public function setOffice(string $office)
     {
-        $this->add('office', $office->getCode());
+        $this->add(
+            'office',
+            $office
+        );
         return $this;
     }
 
@@ -29,12 +43,17 @@ class VatDataTransfer extends Read
      * Sets the code for this vat request.
      *
      * @access public
+     *
      * @param string $code
+     *
      * @return \PhpTwinfield\Request\Read\VatDataTransfer
      */
     public function setCode($code)
     {
-        $this->add('code', $code);
+        $this->add(
+            'code',
+            $code
+        );
         return $this;
     }
 }
